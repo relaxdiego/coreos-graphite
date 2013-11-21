524f63513a6b5bf015ea002d0d87f66e704de54e
Mark Maglana <mmaglana@gmail.com>
2013-11-21 15:25:46 -0800

    Add Graphite dummy data generator

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 docker pull relaxdiego/graphite
+docker pull relaxdiego/graphite_dummy_data
 
 echo
 echo 'Registering Graphite service'
 cp /home/core/share/vagrant/graphite.service /media/state/units/graphite.service
+
+echo 'Registering Graphite Dummy Data service'
+cp /home/core/share/vagrant/dummy_data.service /media/state/units/dummy_data.service
 
 
 echo 'Restarting services'
